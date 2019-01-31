721b555afe65346a28aa860c20b935f967a5f0e9
Zhi Yuan Lim <LimZY@Zhis-MacBook-Pro.local>
2019-01-31 19:30:03 +0800

    fixed

--- ps0/bin/activate.fish
+++ ps0/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/LimZY/Dropbox/University Stuff/Y3S2/PH4419 Computational Physics/Assignments/ps1"
+set -gx VIRTUAL_ENV "/Users/LimZY/Dropbox/University Stuff/Y3S2/PH4419 Computational Physics/Assignments/ps0"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(ps1) "
-            printf "%s%s" "(ps1) " (set_color normal)
+        if test -n "(ps0) "
+            printf "%s%s" "(ps0) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
