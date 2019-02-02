94a36e38931a6dbd210503b194d5bde5f5ade47d
Zhi Yuan Lim <l.zhiyuan12@gmail.com>
2019-02-02 22:31:25 +0800

    quiver plot for field is weird

--- ps0/src/U1640242D_Assignment 0_Question 1.py
+++ ps0/src/U1640242D_Assignment 0_Question 1.py
@@ -6,6 +6,11 @@ def calcPotential(r, Q, X, Y):
     # r = position vector of the point charge
     # Q = charge magnitude of the point charge
     # X, Y = meshgrid coordinates
+
+    # Sanity checks to prevent errors
+    assert X.ndim == Y.ndim == 2
+    assert shape(X) == shape(Y)
+
     rx = r[0]
     ry = r[1]
 
@@ -21,7 +26,68 @@ def calcPotential(r, Q, X, Y):
 
     return V
 
-x = y = linspace(-2, 2, 100)
-X, Y = meshgrid(x,y)
-print(calcPotential([1., 1.], 1., X, Y))
+### Define a function to calculate the field components due to a single point charge
+def calcField(r, Q, X, Y): 
+    # r = position vector of the point charge
+    # Q = charge magnitude of the point charge
+    # X, Y = meshgrid coordinates
+    rx = r[0]
+    ry = r[1]
+
+    # Sanity checks to prevent errors
+    assert X.ndim == Y.ndim == 2
+    assert shape(X) == shape(Y)
+
+    # Calculating required coefficients
+    distSq = (X - rx) ** 2 + (Y - ry) ** 2
+    coeff = 1 / (4 * pi) # Since we take permittivity = 1
+
+    # Setting up result matrices
+    E = zeros(shape(X)) # Magnitude of E-field
+    EX = EY = zeros(shape(X)) # x- , y-components of E-field
+
+    # Calculate E-field
+    E = coeff * Q * distSq
+    
+    # Calculate components
+    EX = E * cos(arctan((Y - ry) / (X - rx)))
+    EY = E * sin(arctan((Y - ry) / (X - rx)))
+    
+    return [EX, EY]
+
+### Define a function to draw the required plots for part a)
+def plotEquipotentialAndField():
+    r = [[-1., -1.], [1., 1.]]
+    q = 1
+
+    xMin = yMin = -2.
+    xMax = yMax = 2.
+    numPtsXpotential = 30
+    numPtsYpotential = 30
+    numPtsXfield = 30
+    numPtsYfield = 30
+
+    x1 = linspace(xMin, xMax, numPtsXpotential)
+    y1 = linspace(yMin, yMax, numPtsYpotential)
+    x2 = linspace(xMin, xMax, numPtsXfield)
+    y2 = linspace(yMin, yMax, numPtsYfield)
+
+    X1, Y1 = meshgrid(x1, y1)
+    X2, Y2 = meshgrid(x2, y2)
+
+    V = zeros(shape(X1))
+    EX = EY = Efield = zeros(shape(X2))
+
+    for i in range(len(r)):
+        V += calcPotential(r[i], q, X1, Y1)
+        Efield = calcField(r[i], q, X2, Y2)
+        EX += Efield[0]
+        EY += Efield[1]
+    
+    plt.figure(1)
+    plt.contour(X1, Y1, V, 20)
+    plt.quiver(X2, Y2, EX, EY)
+    plt.show()
+
+plotEquipotentialAndField()
     
