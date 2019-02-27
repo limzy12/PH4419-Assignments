7d34841697abec0d9fbacda3a5f0f6e265c76276
Zhi Yuan Lim <l.zhiyuan12@gmail.com>
2019-02-28 04:31:24 +0800

    phase portrait for double well added

--- ps1/src/U1640242D_Assignment 1_Question 0.py
+++ ps1/src/U1640242D_Assignment 1_Question 0.py
@@ -5,8 +5,12 @@ import matplotlib.pyplot as plt
 
 import scipy.integrate as integrate
 
+##############
+## Part (a) ##
+##############
+
 ### We define a function that computes the time derivatives at t
-def oscillator(u, t, k, D, m):
+def oscillator(u, t, k, D, m): # k ,D, m are as defined in the differential equation
     x = u[0]
     v = u[1]
     udot = [v, -(D * v + k * x)/m]
@@ -49,9 +53,50 @@ def oscillator_plot(t, m, k, D, u0):
     plt.xlabel(r'Position, $x(t)$')
     plt.ylabel(r'Velocity, $v(t)$')
     plt.title(r'Phase portrait and trajectory of $m\ddot{x} + D\dot{x} + kx = 0$;' + '\n m = ' + str(m) + ', D = ' + str(D) + ', k = '+ str(k) + r', $u_0$ = ' + str(u0))
+    plt.legend(['Trajectory'])
     plt.show()
 
 u0 = [0., pi/2]
 t = linspace(0., 10., 1000)
 oscillator_plot(t, 1., 1., 0., u0)
 
+##############
+## Part (b) ##
+##############
+
+### Define the differential equation describing the dynamics of the double well
+def doubleWell(u, t, m):
+    x = u[0]
+    v = u[1]
+    return [v, (x - x ** 3) / m]
+
+### Function to plot the required plots
+def double_potential_plot(m = 1): 
+    xmax = 5
+    vmax = 5
+    numPts = 40
+    x = linspace(-xmax, xmax, numPts)
+    v = linspace(-vmax, vmax, numPts)
+    X, V = meshgrid(x, v)
+
+    dX = zeros(shape(X))
+    dV = zeros(shape(X))
+
+    for i in range(numPts):
+        for j in range(numPts):
+            u = [X[i,j], V[i,j]]
+            udot = doubleWell(u, 0., m)
+            dX[i,j] = udot[0]
+            dV[i,j] = udot[1]
+
+    ## Normalise the length of the arrows 
+    norm = hypot(dX, dV)
+    dX = dX / norm
+    dV = dV / norm
+
+    ## Plotting phase portrait and trajectory
+    plt.figure(2)
+    plt.quiver(X, V, dX, dV)
+    plt.show()
+
+double_potential_plot()
