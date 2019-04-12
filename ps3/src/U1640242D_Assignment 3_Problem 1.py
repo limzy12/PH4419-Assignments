1c0c00aeb5f9a92150fd16f36a5881d1afa424d7
Zhi Yuan Lim <l.zhiyuan12@gmail.com>
2019-04-12 11:20:51 +0800

    comments added

--- ps3/src/U1640242D_Assignment 3_Problem 1.py
+++ ps3/src/U1640242D_Assignment 3_Problem 1.py
@@ -5,9 +5,11 @@ import numpy.random as rndm
 
 dt = 1
 
-def decayProb(t, halfLife):
+## Function to calculate the decay probability given the half life
+def decayProb(halfLife):
     return 1 - (2 ** (-dt / halfLife))
 
+## Define some constants
 N = 10000
 PbHalfLife = 3.3 * 60
 TlHalfLife = 2.2 * 60
@@ -18,6 +20,7 @@ activeBiCount = []
 TlCount = []
 PbCount = []
 
+## Define the starting amounts of each particle
 Bi213 = 10000
 Tl209 = 0
 Pb209 = 0
@@ -30,33 +33,38 @@ stableBiCount.append(Bi209)
 
 for i in range(N):
     ## Decay from Pb-209 to Bi-213
-    PbDecay = sum(rndm.rand(Pb209) < decayProb(i + 1, PbHalfLife))
-    Pb209 -= PbDecay
+    PbDecay = sum(rndm.rand(Pb209) < decayProb(PbHalfLife))
+    Pb209 -= PbDecay 
     Bi209 += PbDecay
 
     ## Decay from Tl-209 to Pb-209
-    TlDecay = sum(rndm.rand(Tl209) < decayProb(i + 1, TlHalfLife))
+    TlDecay = sum(rndm.rand(Tl209) < decayProb(TlHalfLife))
     Tl209 -= TlDecay 
     Pb209 += TlDecay
      
     ## Decay from Bi-213
-    BiDecay = sum(rndm.rand(Bi213) < decayProb(i + 1, BiHalfLife))
+    BiDecay = sum(rndm.rand(Bi213) < decayProb(BiHalfLife))
     decayToPb = sum(rndm.rand(BiDecay) < 0.9791)
     decayToTl = BiDecay - decayToPb
     Bi213 -= BiDecay
     Pb209 += decayToPb
     Tl209 += decayToTl
 
+    ## Append the current number of particles into an array
     activeBiCount.append(Bi213)
     TlCount.append(Tl209)
     PbCount.append(Pb209)
     stableBiCount.append(Bi209)
 
+## Plotting
 time = arange(0, N + 1)
 plt.figure(1)
 plt.plot(time, activeBiCount, label = 'Bi-213')
 plt.plot(time, TlCount, label = 'Tl-209')
 plt.plot(time, PbCount, label = 'Pb-209')
 plt.plot(time, stableBiCount, label = 'Bi-209')
+plt.ylabel('Number of particles')
+plt.xlabel('Time')
+plt.title('Decay of Bismuth-213')
 plt.legend(loc = 'best')
 plt.show()
